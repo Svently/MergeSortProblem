0d4332a6357f6728c9f1f31d4f3db32413a50ea9
Leonora Tindall <nora@nora.codes>
2019-11-06 10:42:57 -0600

    Create array with randomized values

--- src/edu/beloit/csci335/mergesort/Main.java
+++ src/edu/beloit/csci335/mergesort/Main.java
@@ -1,9 +1,16 @@
 package edu.beloit.csci335.mergesort;
 
-public class Main {
+import java.util.concurrent.ThreadLocalRandom;
 
+public class Main {
+    static final int ARRAY_LENGTH = 10000;
+    static final long ARRAY_MAX_VALUE = 100000000;
+    static long[] myArray;
     public static void main(String[] args) {
-	// write your code here
-    	System.out.println("herlla");
+        myArray = new long[ARRAY_LENGTH];
+	    for (int i = 0; i < ARRAY_LENGTH; i++) {
+            myArray[i] = ThreadLocalRandom.current().nextLong(ARRAY_MAX_VALUE);
+        }
+	    System.out.println(myArray);
     }
 }
