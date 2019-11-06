17e17714c1490937351556d7c48fe7d1dd27671c
Leonora Tindall <nora@nora.codes>
2019-11-06 10:47:28 -0600

    Add method to check if array is sorted.

--- src/edu/beloit/csci335/mergesort/Main.java
+++ src/edu/beloit/csci335/mergesort/Main.java
@@ -11,6 +11,17 @@ public class Main {
 	    for (int i = 0; i < ARRAY_LENGTH; i++) {
             myArray[i] = ThreadLocalRandom.current().nextLong(ARRAY_MAX_VALUE);
         }
-	    System.out.println(myArray);
+	    System.out.println("Before sorting, is the array sorted?: " + isSorted(myArray));
+    }
+
+    private static boolean isSorted(long[] array) {
+        long last;
+        last = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < last) {
+                return false;
+            }
+        }
+        return true;
     }
 }
